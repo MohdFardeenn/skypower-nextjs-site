5117fd3af59964084712858dea02957ddb10d987
Mohd Fardeen <143597570+MohdFardeenn@users.noreply.github.com>
2025-05-04 04:09:52 +0530

    Fix build issues

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import CosmoGreenFeatures from "@/ui/components/Home/CosmoGreenFeatures";
 import CustomerTestimonials from "@/ui/components/Home/CustomerTestimonials";
 import FeaturedProducts from "@/ui/components/Home/FeaturedProducts";
 import LandingPage from "@/ui/components/Home/LandingPage";
-import Image from "next/image";
+// import Image from "next/image";
 
 export default function Home() {
   return(
